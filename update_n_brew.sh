19c59218976212919372a11a5541e594dc2c725e
Kage <2138096+GitKageHub@users.noreply.github.com>
2023-02-15 17:44:08 -0700

    sanity
    added a check for homebrew before executing, installs if not found

--- update_n_brew.sh
+++ update_n_brew.sh
@@ -3,16 +3,22 @@
 # Update the OS
 sudo softwareupdate -ia --verbose
 
-# Update Homebrew and packages managed by Homebrew
+# check for Homebrew and install it if not found
+if [ ! -x "$(command -v brew)" ]; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# update Homebrew and packages managed by Homebrew
 brew update
 brew upgrade
 brew cleanup
 brew doctor
 
-# Update pip and packages managed by pip
+# check for pip and upgrade packages managed by pip if it's installed
 if [ -x "$(command -v pip3)" ]; then
-  pip3 install --upgrade pip
-  pip3 list --outdated | cut -d' ' -f1 | xargs -n1 pip3 install --upgrade
+    pip3 install --upgrade pip
+    pip3 list --outdated | cut -d' ' -f1 | xargs -n1 pip3 install --upgrade
 fi
 
 # Remove deprecated Homebrew packages
